2fa9a67ef20e08e729a443200bd584f9f04c06c1
dulcecallejas <98191749+dulcecallejas@users.noreply.github.com>
2022-03-12 10:53:55 -0600

    Agrego el archivo a GitHub

---
+++ SumaDosEnteros.py
@@ -0,0 +1,24 @@
+#Definicion de la funcion que suma 2 numeros enteros
+def Suma2Enteros (x,y):
+  z = x + y
+   
+  return z
+
+while True:
+  print ("¿Quieres jugar a sumar dos números?")  
+  print ("1 Si")
+  print ("2 No")
+  opcion = int(input("Introduce la opcion==>"))
+
+  if opcion == 1: 
+    x = int(input("Escribe el valor del primer número entero==>"))
+    y = int(input("Escribe el valor del segundo número entero==>"))
+    z = Suma2Enteros(x,y)
+    print (x, " + ", y, " = ", z)
+
+  elif opcion == 2:
+    print ("Bye")
+    break
+  else:
+    print("Opción incorrecta")
+  
